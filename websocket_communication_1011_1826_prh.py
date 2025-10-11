2e9e70b904bc75292f6999d876ccb2b48eb0f34b
z314AHir <viceroy.belief.0d@icloud.com>
2025-10-11 11:26:16 +0100

    Add PYTHON WebSocket实时通信

---
+++ websocket_communication_1011_1826_prh.py
@@ -0,0 +1,5 @@
+# 代码生成时间: 2025-10-11 18:26:15
+from django.core.exceptions import ImproperlyConfigured
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import StopConsumer
+from channels.db import database_sync_to_async"
